f88e5a10de0b112cba71601878bd4cf1b8ab318d
Anton Shiryaev <anton.o.shiryaev@gmail.com>
2018-05-26 22:51:19 +0300

    1

--- main.c
+++ main.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 int main(int argc, char *argv[])
 {
+    int i;
     FILE *f;
-    char s[1035];
+    char s[1024];
+    char commands[1024][1024];
 
-    f = popen("/usr/bin/env crontab -l", "fp");
+    f = popen("/usr/bin/env crontab -l", "r");
     if (f == NULL) 
     {
         printf("Failed\n");
         exit(1);
     }
+    i = 0;
     while (fgets(s, sizeof(s), f) != NULL)
     {
-        if (s[0] != '#')
+        if ((s[0] != '#') && (!strstr(s, "MAILTO=")))
         {
-            printf("%s\n", s);
+            strcpy(commands[i], s);
+            i++;
         }
     }
     return 0;
