c80d580ed937f147ba25188402d7277bbbf2c6f0
Anton Shiryaev <anton.o.shiryaev@gmail.com>
2018-05-27 22:52:28 +0300

    !!

--- main.c
+++ main.c
@@ -5,78 +5,71 @@
 int parse(char *cmd)
 // Returns time interval in seconds
 {
-    int time = 0;
-    char str[3];
-    // Minutes
-    if (strstr(cmd, "*/"))
-    {
-        int period_start, period_end, j = 0;
-        char *e, *t;
-        e = strchr(cmd, '/'); 
-        period_start = (int)(e - cmd);
-        strncpy(t, cmd, period_start); 
-        e = strchr(cmd, ' '); 
-        period_end = (int)(e - cmd);
-        e[0] = '\0';
-        for (int i = period_start; i < period_end; i++)
-        {
-            e[j] = cmd[i];
-            j++;
-        }
-        printf("%s", e);
-        /* time = atoi(e); */
+  int time = 0;
+  // Minutes
+  if (strstr(cmd, "*/")) {
+    int period_start, period_end, j = 0;
+    char *e, *t = '\0';
+    e = strchr(cmd, '/');
+    period_start = (int)(e - cmd);
+    strncpy(t, cmd, period_start);
+    e = strchr(cmd, ' ');
+    period_end = (int)(e - cmd);
+    e[0] = '\0';
+    for (int i = period_start; i < period_end; i++) {
+      e[j] = cmd[i];
+      j++;
     }
-    /* if (cmd[0] != '*') */
-    /* { */
-    /*     strncpy(str, cmd, 2); */
-    /*     str[2] = '\0'; */
-    /*     time += 60 * atoi(str); */
-    /* } */
-    /* // Hours */
-    /* if (cmd[3] != '*') */
-    /* { */
-    /*     strncpy(str, cmd, 2); */
-    /*     str[2] = '\0'; */
-    /*     time += 3600 * atoi(str); */
-    /* } */
-    /* // Days */
-    /* if (cmd[5] != '*') */
-    /* { */
-    /*     strncpy(str, cmd, 2); */
-    /*     str[2] = '\0'; */
-    /*     time += 86400 * atoi(str); */
-    /* } */
-    /* if (cmd[7] != '*') */
-    /* { */
-    /*     strncpy(str, cmd, 2); */
-    /*     str[2] = '\0'; */
-    /*     time += month_len * 86400 * atoi(str); */
-    /* } */
-    return time;
+    printf("%s", e);
+    /* time = atoi(e); */
+  }
+  /* if (cmd[0] != '*') */
+  /* { */
+  /*     strncpy(str, cmd, 2); */
+  /*     str[2] = '\0'; */
+  /*     time += 60 * atoi(str); */
+  /* } */
+  /* // Hours */
+  /* if (cmd[3] != '*') */
+  /* { */
+  /*     strncpy(str, cmd, 2); */
+  /*     str[2] = '\0'; */
+  /*     time += 3600 * atoi(str); */
+  /* } */
+  /* // Days */
+  /* if (cmd[5] != '*') */
+  /* { */
+  /*     strncpy(str, cmd, 2); */
+  /*     str[2] = '\0'; */
+  /*     time += 86400 * atoi(str); */
+  /* } */
+  /* if (cmd[7] != '*') */
+  /* { */
+  /*     strncpy(str, cmd, 2); */
+  /*     str[2] = '\0'; */
+  /*     time += month_len * 86400 * atoi(str); */
+  /* } */
+  return time;
 }
 
-int main(int argc, char *argv[])
-{
-    int i;
-    FILE *f;
-    char s[1024];
-    char cmds[1024][1024];
+int main(int argc, char *argv[]) {
+  int i;
+  FILE *f;
+  char s[1024];
+  char cmds[1024][1024];
 
-    f = popen("/usr/bin/env crontab -l", "r");
-    if (f == NULL) 
-    {
-        printf("Failed\n");
-        exit(1);
+  f = popen("/usr/bin/env crontab -l", "r");
+  if (f == NULL) {
+    printf("Failed\n");
+    exit(1);
+  }
+  i = 0;
+  while (fgets(s, sizeof(s), f) != NULL) {
+    if ((s[0] != '#') && (!strstr(s, "MAILTO="))) {
+      strcpy(cmds[i], s);
+      i++;
     }
-    i = 0;
-    while (fgets(s, sizeof(s), f) != NULL)
-    {
-        if ((s[0] != '#') && (!strstr(s, "MAILTO=")))
-        {
-            strcpy(cmds[i], s);
-            i++;
-        }
-    }
-    parse("*/25");
-    return 0;
+  }
+  parse("*/25");
+  return 0;
 }
