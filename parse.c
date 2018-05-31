57fa322fd503c383db6f264ab786ea07608e4388
Wera Barinova <alisawera@gmail.com>
2018-05-31 20:03:29 +0300

    We can have more than 64 processes when task is running! Assumed 4096

--- parse.c
+++ parse.c
@@ -11,35 +11,10 @@ cron_task parse(char* cmd)
     c.period = c.period * 10 + *period_pointer - '0';
     period_pointer++;
   }
-  /* period_pointer += 1; */ // Two symbols
-  /* period_start = (int)(period_pointer - cmd); */
-  /* period_pointer = strchr(cmd, ' '); */
-  /* period_end = (int)(period_pointer - cmd); */
-  /* period_pointer = (char*)malloc(256); */
-  /* for (int i = period_start; i < period_end; i++) { */
-  /*   period_pointer[j] = cmd[i]; */
-  /*   j++; */
-  /* } */
-  /* c.period = atoi(period_pointer); */
-  /* free(period_pointer); */
-  // Script name
-  /* j = period_end; */
-  /* while ((cmd[j] == ' ') || (cmd[j] == '*')) { */
-  /*  j++; */
-  /* } */
   while ((*period_pointer == ' ') || (*period_pointer == '*')) {
     period_pointer++; // skip stars /* TODO: skip other periods! */
   }
-  /* for (int i = j; i < (int)(sizeof(cmd) / sizeof(char)); i++) {
-    c.p.name[k] = cmd[i];
-    k++;
-  } */
   strcpy(c.p.name, period_pointer);
-  /* while (*period_pointer != '\0') {
-    c.p.name[k] = *period_pointer;
-    *period_pointer++;
-  }
-  c.p.name[k] = '\0'; */
   return c;
 }
 
@@ -49,7 +24,8 @@ void read_crontab(char* user)
   FILE* f;
   char s[1024];
   char exec[256];
-  cron_task cmds[64];
+  cron_task cmds[4096]; // the situation when there are lots of processes is
+  // our main goal; So let's consider, we can have 4 thousand processes!
   snprintf(exec, sizeof(exec),
       "/bin/sh -c \"crontab -u %s -l\"", user);
   f = popen(exec, "r");
